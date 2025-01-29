8381da9e03fd1360e7d49fc924d97be94f2ba057
AlexJara11 <alexanderjaramillo152@gmail.com>
2025-01-29 17:53:40 -0500

    Creación de método de MenuService y creación de clases interfaces y servicios de Rol y Usuario

---
+++ CryptoCurrency.BLL/Servicios/UsuarioService.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using CryptoCurrency.BLL.Servicios.Contrato;
+using CryptoCurrency.DAL.Repositorios.Contrato;
+using CryptoCurrency.DTO;
+using CryptoCurrency.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoCurrency.BLL.Servicios
+{
+    public class UsuarioService : IUsuarioService
+    {
+    }
+}
