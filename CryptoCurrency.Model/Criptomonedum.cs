939b4b9155be423a5219464d6b453d034a7f6177
AlexJara11 <alexanderjaramillo152@gmail.com>
2025-01-29 15:58:46 -0500

    Estructura del proyecto

---
+++ CryptoCurrency.Model/Criptomonedum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CryptoCurrency.Model;
+
+public partial class Criptomonedum
+{
+    public string Codigo { get; set; } = null!;
+
+    public string? Nombre { get; set; }
+
+    public string? Symbol { get; set; }
+
+    public string? Descripcion { get; set; }
+
+    public bool? EsActivo { get; set; }
+}
---
+++ CryptoCurrency.Model/PruebaBgContext.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace CryptoCurrency.Model;
+
+public partial class PruebaBgContext : DbContext
+{
+    public PruebaBgContext()
+    {
+    }
+
+    public PruebaBgContext(DbContextOptions<PruebaBgContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Criptomonedum> Criptomoneda { get; set; }
+
+    public virtual DbSet<Menu> Menus { get; set; }
+
+    public virtual DbSet<MenuRol> MenuRols { get; set; }
+
+    public virtual DbSet<Rol> Rols { get; set; }
+
+    public virtual DbSet<Usuario> Usuarios { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=(local); DataBase=PruebaBG; Trusted_Connection=True; TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Criptomonedum>(entity =>
+        {
+            entity.HasKey(e => e.Codigo).HasName("PK__Criptomo__06370DAD2454B522");
+
+            entity.Property(e => e.Codigo)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Descripcion)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.EsActivo)
+                .HasDefaultValue(true)
+                .HasColumnName("esActivo");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.Symbol)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Menu>(entity =>
+        {
+            entity.HasKey(e => e.IdMenu).HasName("PK__Menu__C26AF483E6BF5908");
+
+            entity.ToTable("Menu");
+
+            entity.Property(e => e.IdMenu).HasColumnName("idMenu");
+            entity.Property(e => e.Icono)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("icono");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("nombre");
+            entity.Property(e => e.Url)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("url");
+        });
+
+        modelBuilder.Entity<MenuRol>(entity =>
+        {
+            entity.HasKey(e => e.IdMenuRol).HasName("PK__MenuRol__9D6D61A464137F5B");
+
+            entity.ToTable("MenuRol");
+
+            entity.Property(e => e.IdMenuRol).HasColumnName("idMenuRol");
+            entity.Property(e => e.IdMenu).HasColumnName("idMenu");
+            entity.Property(e => e.IdRol).HasColumnName("idRol");
+
+            entity.HasOne(d => d.IdMenuNavigation).WithMany(p => p.MenuRols)
+                .HasForeignKey(d => d.IdMenu)
+                .HasConstraintName("FK__MenuRol__idMenu__3C69FB99");
+
+            entity.HasOne(d => d.IdRolNavigation).WithMany(p => p.MenuRols)
+                .HasForeignKey(d => d.IdRol)
+                .HasConstraintName("FK__MenuRol__idRol__3D5E1FD2");
+        });
+
+        modelBuilder.Entity<Rol>(entity =>
+        {
+            entity.HasKey(e => e.IdRol).HasName("PK__Rol__3C872F76FE8DFA03");
+
+            entity.ToTable("Rol");
+
+            entity.Property(e => e.IdRol).HasColumnName("idRol");
+            entity.Property(e => e.FechaRegistro)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime")
+                .HasColumnName("fechaRegistro");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("nombre");
+        });
+
+        modelBuilder.Entity<Usuario>(entity =>
+        {
+            entity.HasKey(e => e.IdUsuario).HasName("PK__Usuario__645723A64A51BB82");
+
+            entity.ToTable("Usuario");
+
+            entity.Property(e => e.IdUsuario).HasColumnName("idUsuario");
+            entity.Property(e => e.Clave)
+                .HasMaxLength(40)
+                .IsUnicode(false)
+                .HasColumnName("clave");
+            entity.Property(e => e.Correo)
+                .HasMaxLength(40)
+                .IsUnicode(false)
+                .HasColumnName("correo");
+            entity.Property(e => e.EsActivo)
+                .HasDefaultValue(true)
+                .HasColumnName("esActivo");
+            entity.Property(e => e.FechaRegistro)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime")
+                .HasColumnName("fechaRegistro");
+            entity.Property(e => e.IdRol).HasColumnName("idRol");
+            entity.Property(e => e.NombreCompleto)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("nombreCompleto");
+
+            entity.HasOne(d => d.IdRolNavigation).WithMany(p => p.Usuarios)
+                .HasForeignKey(d => d.IdRol)
+                .HasConstraintName("FK__Usuario__idRol__403A8C7D");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
