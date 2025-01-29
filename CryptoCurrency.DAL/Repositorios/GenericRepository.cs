45c61fceec087d01631d6a9ca758dbeca8354135
AlexJara11 <alexanderjaramillo152@gmail.com>
2025-01-29 16:45:20 -0500

    Programación de Repositorio Genérico

---
+++ CryptoCurrency.DAL/Repositorios/GenericRepository.cs
@@ -0,0 +1,86 @@
+﻿using CryptoCurrency.DAL.DBContext;
+using CryptoCurrency.DAL.Repositorios.Contrato;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoCurrency.DAL.Repositorios
+{
+    public class GenericRepository<TModelo> : IGenericRepository<TModelo> where TModelo : class
+    {
+        private readonly PruebaBgContext _dbpcontext;
+
+        public GenericRepository(PruebaBgContext dbpcontext)
+        {
+            _dbpcontext = dbpcontext;
+        }
+        public async Task<TModelo> Obtener(Expression<Func<TModelo, bool>> filtro)
+        {
+            try
+            {
+                TModelo modelo = await _dbpcontext.Set<TModelo>().FirstOrDefaultAsync(filtro);
+                return modelo;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public async Task<TModelo> Crear(TModelo modelo)
+        {
+            try
+            {
+                _dbpcontext.Set<TModelo>().Add(modelo);
+                await _dbpcontext.SaveChangesAsync();
+                return modelo;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public async Task<bool> Editar(TModelo modelo)
+        {
+            try
+            {
+                _dbpcontext.Set<TModelo>().Update(modelo);
+                await _dbpcontext.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public async Task<bool> Eliminar(TModelo modelo)
+        {
+            try
+            {
+                _dbpcontext.Set<TModelo>().Remove(modelo);
+                await _dbpcontext.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public async Task<IQueryable<TModelo>> Consultar(Expression<Func<TModelo, bool>> filtro = null)
+        {
+            try
+            {
+                IQueryable<TModelo> queryModelo = filtro == null ? _dbpcontext.Set<TModelo>()
+                                                                 : _dbpcontext.Set<TModelo>().Where(filtro);
+                return queryModelo;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ CryptoCurrency.IOC/Dependencia.cs
@@ -0,0 +1,28 @@
+﻿using CryptoCurrency.DAL.DBContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CryptoCurrency.IOC
+{
+    public static class Dependencia
+    {
+        public static void InyectarDependencias(this IServiceCollection service, IConfiguration configuration)
+        {
+            service.AddDbContext<PruebaBgContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("cadenaSQL"));
+            });
+            service.AddTransient(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            service.AddScoped<IVentaRepository, VentaRepository>();
+            service.AddAutoMapper(typeof(AutoMapperProfile));
+            service.AddScoped<IRolService, RolService>();
+            service.AddScoped<IUsuarioService, UsuarioService>();
+            service.AddScoped<ICategoriaService, CategoriaService>();
+            service.AddScoped<IProductoService, ProductoService>();
+            service.AddScoped<IVentaService, VentaService>();
+            service.AddScoped<IDashBoardService, DashBoardService>();
+            service.AddScoped<IMenuService, MenuService>();
+        }
+    }
+}
